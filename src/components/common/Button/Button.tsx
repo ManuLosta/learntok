9d73428e2d5e9f7fe989742287af396cb770c00a
ManuLosta <mlostalom@gmail.com>
2024-10-26 19:43:33 -0300

    feat: button active state

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -2,29 +2,34 @@ import { cn } from '@/utils/cn';
 import { cva, VariantProps } from 'class-variance-authority';
 
 const buttonVariants = cva(
-  'flex items-center justify-center px-4 py-2 rounded-2xl gap-2 font-bold',
+  'flex items-center justify-center px-4 py-2 rounded-2xl gap-2 font-bold transition-all duration-150',
   {
     variants: {
       variant: {
         default: [
           'bg-primary-500 text-background',
           'disabled:bg-gray-300 disabled:text-gray-600',
+          'active:bg-primary-800'
         ],
         ghost: [
           'bg-primary-100 text-primary-700',
           'disabled:bg-gray-200 disabled:text-gray-400',
+          'active:bg-primary-300'
         ],
         outlined: [
-          'bg-transparent text-primary-500 border-primary-500 border-2',
+          'bg-transparent text-primary-500 border-primary-500 border-2 px-3.5 py-1.5',
           'disabled:border-gray-400 disabled:text-gray-400',
+          'active:border-primary-800 active:text-primary-800'
         ],
         danger: [
           'bg-danger-500 text-background',
           'disabled:bg-gray-300 disabled:text-gray-600',
+          'active:bg-danger-700'
         ],
         'danger-outlined': [
-          'bg-transparent border-danger-500 border-2 text-danger-500',
+          'bg-transparent border-danger-500 border-2 text-danger-500 px-3.5 py-1.5',
           'disabled:border-gray-400 disabled:text-gray-400',
+          'active:border-danger-700 active:text-danger-700'
         ],
       },
     },
