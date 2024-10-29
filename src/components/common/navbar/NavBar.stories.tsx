7eef35b65ab85b2c09b5ad32929beb1f764f17be
ManuLosta <mlostalom@gmail.com>
2024-10-29 15:28:40 -0300

    fix: remove RenderComponent

--- src/components/common/navbar/NavBar.stories.tsx
+++ src/components/common/navbar/NavBar.stories.tsx
@@ -1,17 +1,16 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { NavBar, NavBarProps } from './NavBar';
+import { NavBar } from './NavBar';
 import { HomeIcon } from '@/components/common/icon/HomeIcon';
 import { SearchIcon } from '@/components/common/icon/SearchIcon';
 import { ProfileIcon } from '@/components/common/icon/ProfileIcon';
-import { JSX } from 'react';
 
 const meta = {
   title: 'Common/NavBar',
   component: NavBar,
 
   parameters: {
-    layout: 'centered',
+    layout: 'fullscreen',
   },
 
   tags: ['autodocs'],
@@ -21,14 +20,7 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-const RenderComponent = (args: JSX.IntrinsicAttributes & NavBarProps) => (
-  <div className="min-w-[380px]">
-    <NavBar {...args} />
-  </div>
-);
-
 export const Default: Story = {
-  render: (args) => <RenderComponent {...args} />,
   args: {
     items: [
       {label: 'Home', icon: <HomeIcon size={24} />},
