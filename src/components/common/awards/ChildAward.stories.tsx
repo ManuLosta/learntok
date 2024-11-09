2f85d512868c4e2ac829dcfe0b87a9bc8a4a7ec5
rcastromartinez <rcastromartinez@mail.austral.edu.ar>
2024-11-09 16:15:33 -0300

    comiteljd nl

---
+++ src/components/common/awards/ChildAward.stories.tsx
@@ -0,0 +1,43 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { ChildAward } from './ChildAward';
+
+const meta = {
+  title: 'Common/Awards/ChildAward',
+  component: ChildAward,
+  parameters: {
+    layout: 'centered',
+  },
+
+} satisfies Meta<typeof ChildAward>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Gold: Story = {
+  args: {
+    variant: 'gold',
+    title: 'Gold Award Title',
+    category: 'Gold Category',
+    date: '1 Jan 2024',
+  }
+};
+
+export const Silver: Story = {
+  args: {
+    variant: 'silver',
+    title: 'Silver Award Title',
+    category: 'Silver Category',
+    date: '1 Jan 2024',
+  }
+};
+
+export const Bronze: Story = {
+  args: {
+    variant: 'bronze',
+    title: 'Bronze Award Title',
+    category: 'Bronze Category',
+    date: '1 Jan 2024',
+  }
+};
---
+++ src/components/common/awards/ChildAward.tsx
@@ -0,0 +1,44 @@
+import Image from 'next/image';
+import { cva, VariantProps } from 'class-variance-authority';
+import { cn } from '@/utils/cn';
+
+
+const awardsVariants = cva (
+  'flex items-center justify-center rounded-full p-2',
+  {
+    variants: {
+      variant: {
+        gold: "bg-[#FDCB42] border-[#68560F] border-4",
+        silver: "bg-[#C0C0C0] border-[#808080] border-4",
+        bronze: "bg-[#CD7F32] border-[#8B4513] border-4",
+      }
+    },
+  },
+);
+
+export interface ChildAwardProps extends VariantProps<typeof awardsVariants>{
+  title?: string;
+  category?: string;
+  date?: string;
+}
+
+
+export const ChildAward = ({ variant, title = "Course name", category = "Course category", date = "1 Jan 2024" }: ChildAwardProps) => {
+  return (
+    <div className="flex items-center gap-4 p-4">
+      <div className={cn(awardsVariants({ variant }))}>
+        {variant === 'gold' && <Image src={"/assets/booky-gold.svg"} alt="Gold Award" width={72} height={72} />}
+        {variant === 'silver' && <Image src={"/assets/booky-silver.svg"} alt="Silver Award" width={72} height={72} />}
+        {variant === 'bronze' && <Image src={"/assets/booky-bronze.svg"} alt="Bronze Award" width={72} height={72} />}
+      </div>
+      <div className="ml-2.5">
+        <div className="typography-body-bold text-foreground">{title}</div>
+        <div className="typography-body text-slate-500">{category}</div>
+        <div className="typography-body-small text-slate-500 flex items-center gap-2">
+          <Image src="/assets/calendar-icon.svg" alt="Calendar Icon" width={12} height={12} />
+          {date}
+        </div>
+      </div>
+    </div>
+  );
+};
